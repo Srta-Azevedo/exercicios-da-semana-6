462633f189d6cfe754de2b03530a94b9d90e2030
srta-azevedo <srta_azevedo@icloud.com>
2025-07-15 19:45:22 -0300

    O que você mudou

--- hello.js
+++ hello.js
@@ -2,3 +2,11 @@
 console.log("Hello,Node.js!");
 console.log("Node.js version:", process.version);
 console.log("Current directory:", process.cwd());
+
+//o QUE ACONTECEU AQUI?//
+
+//PS C:\Users\srta_\OneDrive\Área de Trabalho\TUTORIA\EXERCICIOS DA SEMANA 6> node hello.js
+// Hello,Node.js!
+// Node.js version: v22.17.0
+// Current directory: C:\Users\srta_\OneDrive\Área de Trabalho\TUTORIA\EXERCICIOS DA SEMANA 6
+// PS C:\Users\srta_\OneDrive\Área de Trabalho\TUTORIA\EXERCICIOS DA SEMANA 6> //
