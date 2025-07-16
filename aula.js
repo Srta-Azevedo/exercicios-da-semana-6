cb5a0f5a6fa491c9071663e2ff518c44531f42a2
srta-azevedo <srta_azevedo@icloud.com>
2025-07-16 14:05:00 -0300

    Atualizando exercícios da semana 6

--- aula.js
+++ aula.js
@@ -4,11 +4,26 @@ console.log("Aula 1 - JavaScript");
 let nome = "Paula Azevêdo";
 let idade = "39";
 let cidade = "Fortaleza";
+let contato = " 85 98944.3066";
 let profissao = "Biomédica Esteta";
 
 console.log("Nome:", nome);
 console.log("Idade:", idade);
 console.log("Cidade:", cidade);
+console.log("Contato:", contato);
 console.log("Profissão:", profissao);
 
+console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.`);
 
+const carro = {
+    marca: "Volkswagen",
+    modelo: "Fusca",
+    ano: 1986,
+    cor: "preto",
+    preço: 15000
+};
+
+console.log("Carro:", carro);
+
+let ListaDeFrutas = ["maçã", "banana", "laranja", "mamao", "uva", "abacaxi", "morango"];
+console.log("Frutas:", ListaDeFrutas.length);
